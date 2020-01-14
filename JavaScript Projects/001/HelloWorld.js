5fe50853d3b863851b2109a56b8591a0df1e55dd
WGaaraDEV <59893298+WGaaraDEV@users.noreply.github.com>
2020-01-14 17:19:46 -0300

    🦶 Nos primeiros passos...
    Você consegue!

---
+++ JavaScript Projects/001/HelloWorld.js
@@ -0,0 +1,24 @@
+/*
+    Escrito por: 👨‍🔬 Cauã » FridayLab™    
+    Cauã apresenta:
+    Projeto 001 » Hello World!
+*/
+
+console.log("Olá mundo!");
+
+/*
+"Console.log()" é uma função que imprime o valor inserido entre parênteses.
+O "Olá mundo!" é uma típica frase padrão, imprimida nos nossos primeiros códigos, todavia; não há nesseci-
+[...] dade alguma de a imprimir, beleza?
+*/
+
+/*
+Se apegou na linguagem?
+Então eu vou te recomendar alguns Guias sobre o JavaScript!
+MDN web docs » Guia JavaScript {
+    url: "https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Guide"
+},
+Curso em Vídeo {
+    url: "https://www.youtube.com/watch?v=Ptbk2af68e8"
+}
+*/
