1c788745de38c030d07b1e76613b59af86e66c25
Cauã <59893298+WGaaraDEV@users.noreply.github.com>
2020-01-14 17:51:21 -0300

    ✨ Aprenda os Fundamentos da Linguagem!
    Com amor,  de Cauã.

---
+++ JavaScript Projects/002/Fundaments.js
@@ -0,0 +1,27 @@
+/*
+    Escrito por: 👨‍🔬 Cauã » FridayLab™    
+    Cauã apresenta:
+    Projeto 002 » Fundamentos do JavaScript.
+*/
+
+/*
+     Variáveis: 
+        Variáveis são espaços na memória do computador onde você pode armazenar dados. Você
+        começa declarando uma variável com a palavra-chave "var" (sem as aspas), seguida por
+        qualquer nome que você queira nomeá-la, segue exemplo:
+*/
+
+var String;
+String = "Olá mundo!"
+
+/*
+     Nota:
+        Ponto-e-vírgula no fim de uma linha indica onde uma instrução termina; só é
+        absolutamente necessário quando você precisa separar instruções em uma única linha. No
+        entanto, algumas pessoas (como eu) acreditam que é uma boa prática colocá-las no final
+        de cada instrução. Existem outras regras para quando você deve ou não usá-las — consulte {
+        
+          Seu Guia para Ponto-e-vírgula em JavaScript para mais detalhes : "news.codecademy.com/your-guide-to-semicolons-in-javascript"
+          
+        };
+*/
