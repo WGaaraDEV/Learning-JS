e693a30d966adf5f3403e50d6b3c2d65cc332df3
Cauã <59893298+WGaaraDEV@users.noreply.github.com>
2020-01-14 18:26:09 -0300

    ✨ Fundamentos da linguagem, aprenda aqui!
    Com amor, de Cauã.

--- JavaScript Projects/002/Fundaments.js
+++ JavaScript Projects/002/Fundaments.js
@@ -12,7 +12,6 @@
 */
 
 var String;
-String = "Olá mundo!"
 
 /*
      Nota:
@@ -25,3 +24,79 @@ String = "Olá mundo!"
           
         };
 */
+
+/*
+     Outra nota:
+        JavaScript é "case sensitive" (ele nota diferença entre maiúsculo e minúsculo) — Note que
+        String é diferente de string, ou STRING. Se você tiver problemas com o seu código, cheque isso!
+*/
+
+/* Depois de declarar uma variável, você pode dar a ela um valor: */
+
+    String = "Olá mundo!"
+/* Você pode fazer as duas operações na mesma linha se quiser: */
+    var Variável = "Olá mundo!"
+/* Depois de dar à variável um valor, você pode mudá-lo: */
+    var minhaVariável = "Bob"
+    minhaVariável = "Steve"
+/* Note que as variáveis têm diferentes tipos de dados: */
+
+/*
+     String:
+        Uma sequência de texto é conhecida como uma string. Para mostrar que o
+        valor é uma string, você deve envolvê-la em aspas.
+     Exemplo:
+     var myVariable = 'Hello World!';
+*/
+
+
+/*
+     Number:
+        Um número. Números não têm aspas ao redor deles.
+     Exemplo:
+     var myNumber = 2;
+*/
+
+
+/*
+     Boolean:
+        Um número. Números não têm aspas ao redor deles.
+     Exemplo:
+     var myNumber = 2;
+*/
+
+
+/*
+     Array:
+        Uma estrutura que permite armazenar vários valores em uma única variável.
+     Exemplo:
+     var myArray = [
+     1,
+     'Bob',
+     'Steve',
+     'Tony',
+     10];
+     Acesse cada item desse array dessa maneira:
+     myArray[0]; retorna o número 1 (o primeiro valor da array).
+     myArray[1]; retorna 'Bob' (o segundo valor da array).
+*/
+
+
+/*
+     Object:
+        Basicamente, qualquer coisa. Tudo em JavaScript é um objeto e pode ser
+        armazenado em uma variável. Tenha isso em mente enquanto aprente.
+     Exemplo, apenas funciona em Web:
+     var myTest = document.querySelector('h1'); 
+     Todos os exemplos acima também.
+*/
+
+/*
+     Propósito das Variáveis:
+        Já se perguntou o motivo de usarmos variáveis? Bom, variáveis são necessárias
+        para fazer qualquer coisa interessante em programação. Se os valores não pudessem
+        mudar, então você não poderia fazer nada dinâmico, como personalizar uma mensagem
+        de boas-vindas, ou mudar a imagem mostrada em uma galeria de imagens.
+*/
+
+/* Continua... */
